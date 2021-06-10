a6056c92d3dd777194baeb1520a6a6b68add42a6
valouugit <valentingoulier@gmail.com>
2021-06-10 11:47:05 +0200

    Optimisation du login à la bdd Ajout de commentaires Retrait des liaison avec le BDE

--- API/core/data.py
+++ API/core/data.py
@@ -5,36 +5,41 @@ class ShibaData():
 
     def __init__(self, email, ville):
         self.email = email
-        self.ville = ville
-        self.db = sqlite3.connect('db/cesi.db')
+        self.db = sqlite3.connect('/home/valouu/Documents/shiba/API/db/cesi.db')
         self.cursor = self.db.cursor()
-        self.wallet, self.manager = None, None
+
+    """
+    
+        Permet de se connecter à la bdd de manière dynamique,
+        Création du compte automatiquement si il n'existe pas encore,
+        La fonction doit être utilisée si l'authentification est déjà réalisée
+
+    """
 
     def login(self):
         
+        # Extrait le nom/prénom depuis l'email
+
         def name_split():
             names = self.email.split('@')
             names = names[0].split('.')
             prenom, nom = names
             return prenom.title(), nom.upper()
 
+        # Push le compte dans la base de données
+        # {prenom_nom} tableau avec ['prenom', 'nom'] venant de la fonction name_split()
+
+        def register_in_db(prenom_nom , bde):
+            self.cursor.execute("INSERT INTO USER ('LastName','FirstName','Email') VALUES ('%s', '%s', '%s')" % (prenom_nom[0], prenom_nom[1], self.email))
+            self.db.commit()
+
+        # Récupere les données de l'utilisateur
+        # {result} retourne 'None' si le compte est non engregistré
 
         result = self.cursor.execute("SELECT Manager, Wallet FROM USER WHERE email='%s'" % self.email).fetchone()
         
         if result == None:
 
-            result = self.cursor.execute("SELECT ID_BDE FROM BDE WHERE city='%s'" % self.ville).fetchone()
-            if result == None:
-                bde = 0
-            else:
-                bde = result[0]
+            # Enregistre le nouveau compte dans la base de données
 
-            prenom, nom = name_split()
-
-            self.cursor.execute("INSERT INTO USER ('LastName','FirstName','Email','ID_BDE', 'Manager', 'Wallet') VALUES ('%s', '%s', '%s', '%s', 'False', 0)" % (nom, prenom, self.email, bde))
-            self.db.commit()
-
-        else:
-            self.manager = True if result[0] == 'True' else False
-            self.wallet = result[1]
-            return True
+            register_in_db(name_split())
